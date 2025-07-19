3ec1704c63b596584555d2c987935be27a6db609
johan-joseph123 <gamailmejoanz@gmail.com>
2025-07-19 21:00:09 +0530

    even num added

---
+++ src/M1/evennumwhile.java
@@ -0,0 +1,12 @@
+package M1;
+
+public class evennumwhile {
+    public static void main(String[] args){
+        int i=2;
+        while(i<=20){
+            System.out.println(i);
+            i+=2;
+        }
+    }
+    
+}
